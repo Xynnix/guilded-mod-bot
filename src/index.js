854082309e6945b79acdfd450b4d341992cd5142
NightRaven <53287573+Xynnix@users.noreply.github.com>
2020-11-14 22:53:45 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
+const Enmap = require("enmap")
+const fs = require("fs")
 const Guilded = require('guilded.js');
 const client = new Guilded.Client();
+const dotenv = require("dotenv").config();
 
 client.on("ready", () => {
   console.log("ready");
 });
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/${file}`)];
+  });
+});
+client.commands = new Enmap();
 
-client.on("message", (msg) => {
-  var text = msg.content[0].text;
- //get the message content for line 1
-  if(!text) return;
-  
-  if(text == "ping") {
-    msg.channel.send("pong");
-  }
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
 });
 
 client.login(process.env.email, process.env.password);
